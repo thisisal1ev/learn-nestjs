23e20c876d725375cd0314f397fd71a38cc7c098
thisisal1ev <aaalievvv1@gmail.com>
2025-02-13 17:25:03 +0500

    feat: integrate Prisma for database access and add Flower model

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 export class FlowersService {
+	constructor(private readonly prisma: PrismaService) { }
+
 	getAll() {
-		return [
-			{
-				name: 'Rose',
-				color: 'Red',
-				price: 5
-			},
-			{
-				name: 'Lily',
-				color: 'White',
-				price: 6
-			},
-			{
-				name: 'Tulip',
-				color: 'Yellow',
-				price: 7
-			},
-		]
+		return this.prisma.flower.findMany()
+
+		// return [
+		// 	{
+		// 		name: 'Rose',
+		// 		color: 'Red',
+		// 		price: 5
+		// 	},
+		// 	{
+		// 		name: 'Lily',
+		// 		color: 'White',
+		// 		price: 6
+		// 	},
+		// 	{
+		// 		name: 'Tulip',
+		// 		color: 'Yellow',
+		// 		price: 7
+		// 	},
+		// ]
 	}
 }
